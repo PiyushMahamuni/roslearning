f846f664882daa87c6519285c37d3e06191f6f9c
Piyush Mahamuni <mahamunipiyush@outlook.com>
2021-10-13 14:17:25 +0530

    Refactored, commented

--- src/Navigation/Map_Based/_01Set_Nav_Goal.py
+++ src/Navigation/Map_Based/_01Set_Nav_Goal.py
@@ -33,18 +33,30 @@ def setup():
 # THIS FUNCTION WILL MAKE THE ROBOT MOVE
 def move_to_goal(xGoal, yGoal):
     while not ac.wait_for_server(rospy.Duration.from_sec(0.5)):
+        # waits for server for given time, returns true if server has come up
         rospy.loginfo("Waiting for the move_base action server to come up")
         if rospy.is_shutdown():
             rospy.loginfo(f"Ternimating the {NODE_NAME} node")
             sys.exit()
     goal = MoveBaseGoal()
+    # The message that will be sent as request to the server by the action client - `ac`
 
     # SET UP THE FRAME PARAMETERS
     goal.target_pose.header.frame_id = "map"
+    # the name of the frame in which or for which you are defining the goal pose
+    # you need to set the frame_id to the correct value for proper navigation of robot.
     goal.target_pose.header.stamp = rospy.Time.now()
+    # returns the current time as a time stamp.
 
-    # MOVING TOWARDS THE GOAL   
-    goal.target_pose.pose.position = Point(xGoal, yGoal, 0)
+    # MOVING TOWARDS THE GOAL
+    # defining the goal position
+    goal.target_pose.pose.position.x = xGoal
+    goal.target_pose.pose.position.y = yGoal
+    goal.target_pose.pose.position.z = 0
+    # ALTERNATIVELY
+    # goal.target_pose.pose.position = Point(xGoal, yGoal, 0)
+
+    # defining the goal orientation
     goal.target_pose.pose.orientation.x = goal.target_pose.pose.orientation.y = goal.target_pose.pose.orientation.z = 0.0
     goal.target_pose.pose.orientation.w = 1.0
 
@@ -52,6 +64,8 @@ def move_to_goal(xGoal, yGoal):
 
     ac.send_goal(goal)
     ac.wait_for_result(rospy.Duration(60))
+    # wait for 60 seconds for robot to complete its navigation mission and then
+    # check the state of the request made by the action client
     
     if ac.get_state() == actionlib_msgs.msg.GoalStatus.SUCCEEDED:
         rospy.loginfo("Robot reached the destination")
