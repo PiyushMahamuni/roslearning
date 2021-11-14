98ae2f2e346f0015edef851a045eea7a4b5372ef
Piyush Mahamuni <mahamunipiyush@outlook.com>
2021-11-14 19:51:48 +0530

    Refactored

--- src/Control/PID/04id.cpp
+++ src/Control/PID/04id.cpp
@@ -4,8 +4,8 @@
 
 // CONSTANTS
 const char* NODE_NAME{"idLinController"};
-const char* VEL_TOPIC{"turtle1/cmd_vel"};
-const char* POSE_TOPIC{"turtle1/pose"};
+const char* VEL_TOPIC{"/turtle1/cmd_vel"};
+const char* POSE_TOPIC{"/turtle1/pose"};
 const _Float32 LT {0.01}; // linar tolerance
 const float blinkFreq {100.0};
 const float controllerFreq{100.0};
@@ -20,9 +20,12 @@ geometry_msgs::Twist stop_msg;  // velocity message having all fields set to zer
 bool poseUpdated {false};   // tells whether position is updated since the last time this variable
 // was set to false
 ros::Rate* blink{nullptr};   // blinking (short idle) duration
+ros::NodeHandle* node{nullptr};
 
 // setup this node
 inline void setup(int argc, char** argv);
+// wrap up this node
+inline void wrapup();
 // callback function for pose_sub
 void pose_callback(const turtlesim::Pose::ConstPtr& msg);
 // function implementing integral-derivative linear controller
@@ -33,19 +36,27 @@ void stop_robot();
 void waitPoseUpdate();
 
 int main(int argc, char** argv){
-    if(argc != 5){
-        std::cerr << "Usage: " << argv[0] << " [x, m, kid, kdd]\n";
-        std::cout << "Info -------------------------" << std::endl
-        << "This program works along with turtlesim_node, emulating that robot has\n"
-        << "Given mass `m` it implements a integral-derrivative cotroller whose output is a `force`\n"
-        << "Which is imparted on body of robot to make it move\n"
-        << "Note, this program doesn't take in account the angle of robot and assumes it is at 0 rad\n";
-        
+    setup(argc, argv);
+    std::cerr << "Usage: " << argv[0] << " [x, m, kid, kdd]\n";
+    std::cout << "Info -------------------------" << std::endl
+    << "This program works along with turtlesim_node, emulating that robot has\n"
+    << "Given mass `m` it implements a integral-derrivative cotroller whose output is a `force`\n"
+    << "Which is imparted on body of robot to make it move\n"
+    << "Note, this program doesn't take in account the angle of robot and assumes it is at 0 rad\n";
+    float x0, m, kid, kdd;
+    bool success{true};
+    success = success && node->getParam("x0", x0);
+    if(success)
+        success = success && node->getParam("m", m);
+    if(success)
+        success = success && node->getParam("kid", kid);
+    if(success)
+        success = success &&node->getParam("kdd", kdd);
+    if(!success){
+        ROS_INFO("[%s] Couldn't Retrieve all the parameters!", NODE_NAME);
         return 1;
     }
-    setup(argc, argv);
-    idLineController((_Float32)atof(argv[1]), (_Float32)atof(argv[3]), 
-                     (_Float32)atof(argv[4]), (_Float32)atof(argv[2]), true);
+    idLineController(x0, kid, kdd, m, true);
     return 0;
 }
 
@@ -56,13 +67,20 @@ inline void setup(int argc, char** argv){
     vel_msg.angular.x = vel_msg.angular.y = vel_msg.angular.z = 0;
     stop_msg = vel_msg;
     ros::init(argc, argv, NODE_NAME);
-    ros::NodeHandle node;
+    node = new ros::NodeHandle("~");
     // initialize POSE_TOPIC subscriber
-    pose_sub = node.subscribe(POSE_TOPIC, 1, pose_callback);
+    pose_sub = node->subscribe(POSE_TOPIC, 1, pose_callback);
     // initialize VEL_TOPIC publisher
-    vel_pub = node.advertise<geometry_msgs::Twist>(VEL_TOPIC, 1);
+    vel_pub = node->advertise<geometry_msgs::Twist>(VEL_TOPIC, 1);
     // initialize blink
     blink = new ros::Rate(blinkFreq);
+    return;
+}
+
+// wrapup this node
+inline void wrapup(){
+    delete blink;
+    delete node;
     return;
 }
 
