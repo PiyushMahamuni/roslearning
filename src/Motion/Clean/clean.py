4263e686890c0ae422b66e9c59a911eab9a50b54
Piyush Mahamuni <mahamunipiyush@outlook.com>
2021-11-14 23:10:19 +0530

    Refactored

--- src/Motion/Clean/clean.py
+++ src/Motion/Clean/clean.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 
-from typing import List
 import rospy, sys, math, time
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
@@ -15,18 +14,20 @@ LIMITS = [0.5, 11-0.5]
 
 
 # GLOBALS
-vel_pub = None
-pose_sub = None
-cpos = None
+vel_pub: rospy.Publisher = None
+pose_sub: rospy.Subscriber = None
+cpos: Pose = None
 
 # callback for POSE_TOPIC
 def pose_callback(msg):
+    """updates global `cpos: Pose` variable"""
     global cpos
     cpos = msg
 
 
 # setup this node
 def setup():
+    """Setup the velocity publisher and position subscriber"""
     rospy.init_node(NODE_NAME)
     global vel_pub, pose_sub
     vel_pub = rospy.Publisher(VEL_TOPIC, Twist, queue_size=1)
@@ -34,8 +35,9 @@ def setup():
 
 
 def stop_robot():
-    vel_msg = Twist()
-    vel_pub.publish(vel_msg)
+    """Stops the robot"""
+    vel_cmd = Twist()
+    vel_pub.publish(vel_cmd)
 
 
 def move(distance: float, speed: float, log: bool = False) -> bool:
@@ -50,8 +52,8 @@ def move(distance: float, speed: float, log: bool = False) -> bool:
         rospy.logerr(f"[{NODE_NAME}] Can't complete requested command, robot will crash")
         return False
     speed = abs(speed) if distance > 0 else -abs(speed)
-    vel_msg = Twist()
-    vel_msg.linear.x = speed
+    vel_cmd = Twist()
+    vel_cmd.linear.x = speed
     if log:
         rospy.loginfo(f"""[{NODE_NAME}] Command requested to move the robot {distance} distance
         with {speed} speed""")
@@ -66,21 +68,21 @@ def move(distance: float, speed: float, log: bool = False) -> bool:
         if cpos.y > ty:
             while cpos.y > ty:
                 loop_rate.sleep()
-                vel_pub.publish(vel_msg)
+                vel_pub.publish(vel_cmd)
         else:
             while cpos.y < ty:
                 loop_rate.sleep()
-                vel_pub.publish(vel_msg)
+                vel_pub.publish(vel_cmd)
     else:
         # use x cordinate
         if cpos.x > tx:
             while cpos.x > tx:
                 loop_rate.sleep()
-                vel_pub.publish(vel_msg)
+                vel_pub.publish(vel_cmd)
         else:
             while cpos.x < tx:
                 loop_rate.sleep()
-                vel_pub.publish(vel_msg)
+                vel_pub.publish(vel_cmd)
     stop_robot()
     if log:
         rospy.loginfo(f"[{NODE_NAME}] moveto command completed!")
@@ -89,12 +91,12 @@ def move(distance: float, speed: float, log: bool = False) -> bool:
 
 #turn by a specific angle radians with given speed
 def turn(radians: float, speed: float) -> None:
+    """Turns robot through given radians with given speed"""
     stop_robot()
     rospy.wait_for_message(POSE_TOPIC, Pose)
     loop_freq = 20
     loop_rate = rospy.Rate(loop_freq)
     speed = abs(speed)
-    cclk = True if radians > 0 else False
     radians = abs(radians)
     loop_dur = loop_freq**-1
     total_time = radians / speed
@@ -103,20 +105,21 @@ def turn(radians: float, speed: float) -> None:
         loop_count += 1
     loop_count -= 1
     rem_time = total_time - loop_count * loop_dur
-    vel_msg = Twist()
-    vel_msg.angular.z = speed if cclk else -speed
+    vel_cmd = Twist()
+    vel_cmd.angular.z = speed if radians > 0 else -speed
     while loop_count:
-        vel_pub.publish(vel_msg)
+        vel_pub.publish(vel_cmd)
         loop_count -= 1
         loop_rate.sleep()
-    vel_pub.publish(vel_msg)
+    vel_pub.publish(vel_cmd)
     time.sleep(rem_time)
-    vel_msg.angular.z = 0
-    vel_pub.publish(vel_msg)
+    vel_cmd.angular.z = 0
+    vel_pub.publish(vel_cmd)
 
 
 #turnto specific angle in radians
 def turntor(radians: float, speed: float, log: bool = False) -> None:
+    """Turns the robot facing towards a specific angle"""
     stop_robot()
     rospy.wait_for_message(POSE_TOPIC, Pose)
     angle_to_traverse = radians - cpos.theta
@@ -136,50 +139,49 @@ def turntor(radians: float, speed: float, log: bool = False) -> None:
 
 
 # goto specific location x, y
-def goto(x, y, kpd=0.5, kpa=2):
+def goto(x, y, kpd=0.5, kpa=2) -> bool:
+    """Makes the robot move to a specific location, returns true or false based on wheter
+    the operation was successful or not"""
     rospy.wait_for_message(POSE_TOPIC, Pose)
-    vel_msg = Twist()
+    vel_cmd = Twist()
     loop_rate = rospy.Rate(50)
     while not rospy.is_shutdown():
         xE = x - cpos.x
         yE = y - cpos.y
         distE = math.sqrt(xE**2 + yE**2)
         if(distE < 0.01):
-            break
-        vel_msg.linear.x = kpd * distE
+            return True
+        vel_cmd.linear.x = kpd * distE
         angleE = math.asin(yE/distE)
         if x < cpos.x:
-            if angleE > 0:
-                angleE = math.pi - angleE
-            else:
-                angleE = -math.pi - angleE
+            angleE = math.pi * (1 if yE > 0 else -1) - angleE
         angleE -= cpos.theta
-        if angleE > math.pi:
-            angleE = angleE - 2 * math.pi
-        elif angleE < -math.pi:
-            angleE = angleE + 2 * math.pi
+        angleE -= 2 * math.pi * (1 if angleE > math.pi else (-1 if angleE < -math.pi else angleE))
 
-        vel_msg.angular.z = kpa * angleE
+        vel_cmd.angular.z = kpa * angleE
         loop_rate.sleep()
-        vel_pub.publish(vel_msg)
+        vel_pub.publish(vel_cmd)
+    return False
 
 
 # move in a spiral tragectory
-def spiral_clean(rk = 0.001):
+def spiral_clean(rk: float = 0.001) -> None:
+    """moves the robot in a spiral trajectory until it gets too close to the walls"""
     stop_robot()
     loop_freq = 20
     loop_rate = rospy.Rate(loop_freq)
-    vel_msg = Twist()
-    vel_msg.angular.z = math.pi / 4  # 45 degrees per second
+    vel_cmd = Twist()
+    vel_cmd.angular.z = math.pi / 4  # 45 degrees per second
     while cpos.x < LIMITS[1] and cpos.x > LIMITS[0] and cpos.y < LIMITS[1] and cpos.y > LIMITS[0]:
-        vel_msg.linear.x += rk
-        vel_pub.publish(vel_msg)
+        vel_cmd.linear.x += rk
+        vel_pub.publish(vel_cmd)
         loop_rate.sleep()
     stop_robot()
 
 
 # move in a grid pattern
-def grid_clean():
+def grid_clean() -> None:
+    """Moves the robot in a grid-like pattern"""
     goto(1, 1)
     speed = math.pi / 4
     turntor(0, speed)
@@ -189,7 +191,6 @@ def grid_clean():
         move(9, 1)
         turntor(0, speed)
         alt_angle *= -1
-
 
 
 if __name__ == "__main__":
@@ -204,5 +205,3 @@ if __name__ == "__main__":
     else:
         print("Sorry, Invalide option")
         sys.exit(1)
-    
-    
