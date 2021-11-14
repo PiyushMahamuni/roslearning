fa53fb767b3fe53b3e440390ae40e360ce23ca43
Piyush Mahamuni <mahamunipiyush@outlook.com>
2021-11-14 17:00:15 +0530

    Integral Control, Refactored

--- src/Control/PID/02integral.cpp
+++ src/Control/PID/02integral.cpp
@@ -4,10 +4,12 @@
 #include <math.h>
 
 // CONSTANTS
-const char* NODE_NAME{"pLinController"};
-const char* VEL_TOPIC{"turtle1/cmd_vel"};
-const char* POSE_TOPIC{"turtle1/pose"};
-const _Float32 LT {0.01}; // linar tolerance
+const char* NODE_NAME{"iLinController"};
+const char* VEL_TOPIC{"/turtle1/cmd_vel"};
+const char* POSE_TOPIC{"/turtle1/pose"};
+// NOTE- with private nodehandle, topics, services with relative name are advertised in the namespace of
+// the nodehanlde, therefore, we need to use the absolute names of the topics here.
+const _Float32 LT {0.01}; // linear threshold
 const float blinkFreq {100.0};
 const float controllerFreq{100.0};
 
@@ -21,9 +23,12 @@ geometry_msgs::Twist stop_msg;  // velocity message having all fields set to zer
 bool poseUpdated {false};   // tells whether position is updated since the last time this variable
 // was set to false
 ros::Rate* blink{nullptr};   // blinking (short idle) duration
+ros::NodeHandle* node{nullptr};
 
 // setup this node
 inline void setup(int argc, char** argv);
+// wrap up this node
+inline void wrapup();
 // callback function for pose_sub
 void pose_callback(const turtlesim::Pose::ConstPtr& msg);
 // function implementing integral linear controller
@@ -33,21 +38,32 @@ void stop_robot();
 // wait for pose updated
 void waitPoseUpdate();
 
+
 int main(int argc, char** argv){
-    if(argc != 4){
-        std::cerr << "Usage: " << argv[0] << " [x, m, kpd]\n";
-        std::cout << "Info -------------------------" << std::endl
-        << "This program works along with turtlesim_node, emulating that robot has\n"
-        << "Given mass `m` it implements a integral cotroller whose output is a `force`\n"
-        << "Which is imparted on body of robot to make it move\n"
-        << "Note, this program doesn't take in account the angle of robot and assumes it is at 0 rad\n";
-        
+    std::cout << "Info -------------------------" << std::endl
+    << "This program works along with turtlesim_node, emulating that robot has\n"
+    << "Given mass `m` it implements a integral cotroller whose output is a `force`\n"
+    << "Which is imparted on body of robot to make it move\n"
+    << "Note, this program doesn't take in account the angle of robot and assumes it is at 0 rad\n";
+    setup(argc, argv);
+    float x0, m, kid;
+    bool success{true};
+    success = success && node->getParam("x0", x0);
+    if(success)
+        success = success && node->getParam("m", m);
+    if(success)
+        success = success && node->getParam("kid", kid);
+    if(!success){
+        ROS_INFO("[%s] Failed to retrieve parameters!\n", NODE_NAME);
+        ros::shutdown();
         return 1;
     }
-    setup(argc, argv);
-    iLineController((_Float32)atof(argv[1]), (_Float32)atof(argv[3]), (_Float32)atof(argv[2]), true);
+
+    iLineController(x0, kid, m, true);
+    wrapup();
     return 0;
 }
+
 
 // setup this node
 inline void setup(int argc, char** argv){
@@ -56,15 +72,26 @@ inline void setup(int argc, char** argv){
     vel_msg.angular.x = vel_msg.angular.y = vel_msg.angular.z = 0;
     stop_msg = vel_msg;
     ros::init(argc, argv, NODE_NAME);
-    ros::NodeHandle node;
+    node = new ros::NodeHandle("~");
+    // using "~" arg to declare that this will be a private nodehandle and with that, 
+    // we can access private parameters with NodeHandle::getPrama()
+
     // initialize POSE_TOPIC subscriber
-    pose_sub = node.subscribe(POSE_TOPIC, 1, pose_callback);
+    pose_sub = node->subscribe(POSE_TOPIC, 1, pose_callback);
     // initialize VEL_TOPIC publisher
-    vel_pub = node.advertise<geometry_msgs::Twist>(VEL_TOPIC, 1);
+    vel_pub = node->advertise<geometry_msgs::Twist>(VEL_TOPIC, 1);
     // initialize blink
     blink = new ros::Rate(blinkFreq);
     return;
 }
+
+
+// wrapup this node
+inline void wrapup(){
+    delete blink;
+    delete node;
+}
+
 
 // callback function for pose_sub
 void pose_callback(const turtlesim::Pose::ConstPtr& msg){
@@ -98,9 +125,10 @@ void iLineController(_Float32 x, _Float32 kid, _Float32 m, bool log){
     */
     // ~playground
     _Float32 dx {x - cpos.x}, pv{0}, cv{}, 
-             force{}, K{loop_dur/(2*m)}; // prev velocity, current velocity
+             force{}, K{loop_dur/(2*m)}, dforce{}; // prev change in velocity, change in velocity
     do{
-        force += kid * dx;
+        dforce += kid * dx;
+        force += dforce;
         cv = force * K;
         vel_msg.linear.x += (pv + cv);
         pv = cv;
@@ -123,7 +151,12 @@ void stop_robot(){
 void waitPoseUpdate(){
     poseUpdated = false;
     while(!poseUpdated){
+        try{
         blink->sleep();
         ros::spinOnce();
+        }
+        catch(ros::Exception& e){
+            ROS_INFO("[%s] Exception: %s", NODE_NAME, e.what());
+        }
     }
 }
